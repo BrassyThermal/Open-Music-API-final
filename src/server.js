7b57104c3b10cf8cddb473d1413892db5beb3045
Jadylixx <muhammadyusrildevj@gmail.com>
2023-08-05 17:52:42 +0700

    submission 2

--- src/server.js
+++ src/server.js
@@ -1,48 +1,10 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
-const Jwt = require('@hapi/jwt');
-
-const albums = require('./api/albums');
-const songs = require('./api/songs');
-const AlbumService = require('./services/postgres/albumService');
-const AlbumValidator = require('./validator/albums');
-const SongService = require('./services/postgres/songService');
-const SongValidator = require('./validator/songs');
+const { registerPlugins } = require('./plugins');
 const ClientError = require('./exceptions/clientError');
 
-const users = require('./api/users');
-const UserService = require('./services/postgres/userService');
-const UserValidator = require('./validator/users');
-
-// authentications
-const auths = require('./api/auths');
-const AuthService = require('./services/postgres/authService');
-const tokenManager = require('./tokenize/tokenManager');
-const AuthValidator = require('./validator/auths');
-
-// collaborations
-const collabs = require('./api/collabs');
-const CollabService = require('./services/postgres/CollabService');
-const CollabValidator = require('./validator/collabs');
-
-// playlists
-const playlists = require('./api/playlists');
-const PlaylistService = require('./services/postgres/playlistService');
-const PlaylistSongService = require('./services/postgres/playlistServiceSong');
-const PlaylistActivity = require('./services/postgres/playlistServiceActivity');
-const PlaylistValidator = require('./validator/playlists');
-
 const init = async () => {
-  const albumService = new AlbumService();
-  const songService = new SongService();
-  const userService = new UserService();
-  const authService = new AuthService();
-  const collabService = new CollabService();
-  const playlistActivity = new PlaylistActivity();
-  const playlistService = new PlaylistService(collabService);
-  const playlistSongService = new PlaylistSongService(playlistActivity);
-
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -53,78 +15,7 @@ const init = async () => {
     },
   });
 
-  await server.register([
-    {
-      plugin: Jwt,
-    },
-  ]);
-
-  server.auth.strategy('openmusic_jwt', 'jwt', {
-    keys: process.env.ACCESS_TOKEN_KEY,
-    verify: {
-      aud: false,
-      iss: false,
-      sub: false,
-      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
-    },
-    validate: (artifacts) => ({
-      isValid: true,
-      credentials: {
-        id: artifacts.decoded.payload.id,
-      },
-    }),
-  });
-
-  await server.register([
-    {
-      plugin: albums,
-      options: {
-        albumService,
-        AlbumValidator,
-      },
-    },
-    {
-      plugin: songs,
-      options: {
-        songService,
-        SongValidator,
-      },
-    },
-    {
-      plugin: users,
-      options: {
-        userService,
-        UserValidator,
-      },
-    },
-    {
-      plugin: auths,
-      options: {
-        authService,
-        userService,
-        tokenManager,
-        AuthValidator,
-      },
-    },
-    {
-      plugin: playlists,
-      options: {
-        playlistService,
-        playlistSongService,
-        playlistActivity,
-        PlaylistValidator,
-      },
-    },
-    {
-      plugin: collabs,
-      options: {
-        collabService,
-        playlistService,
-        userService,
-        CollabValidator,
-      },
-    },
-  ]);
+  await registerPlugins(server);
 
   server.ext('onPreResponse', (request, h) => {
     const { response } = request;
